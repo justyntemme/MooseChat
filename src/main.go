a2cfa6a388a48389965c117029b10597425b18c2
Justyn Temme <justyntemme@gmail.com>
2017-01-12 01:07:59 -0600

    framework for toml config file

--- src/main.go
+++ src/main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"fmt"
 
+	"github.com/burntsushi/toml"
 	"github.com/justyntemme/MooseChat/src/tor"
 )
 
 func main() {
 	onionURL := tor.StartTorService()
 	fmt.Println(onionURL)
+}
+
+func readConfig() {
+
 }
